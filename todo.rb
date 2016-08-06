5b552bdf0c10aef7ff3aa0e2185cfd3a5cad43d4
Lisa Hunt <Lisa.Meris.Hunt@gmail.com>
2016-08-06 18:55:35 -0400

    User Story 1: As a user I can create a Todo List

---
+++ todo.rb
@@ -0,0 +1,22 @@
+#Suggested Classes and Methods:
+#
+#    List Class
+#        Create a list
+#        Add task to list
+#        Show all tasks
+#        Read a task from a file
+#        Write a list to a file
+#        Delete a task
+#        Update a task
+#    Task Class
+#        Create a task item
+
+class List
+	def initialize
+	end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  list = List.new
+  puts 'You have created a new list'
+end 
