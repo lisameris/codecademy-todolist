e0f00f20507d0a0159e0c601850e9549b9f975ed
Lisa Hunt <Lisa.Meris.Hunt@gmail.com>
2016-08-07 14:46:14 -0400

    User Story 3: As a user I can see all the tasks in a list

--- todo.rb
+++ todo.rb
@@ -1,9 +1,9 @@
 #Suggested Classes and Methods:
 #
 #    List Class
-#        Create a list
-#        Add task to list
-#        Show all tasks
+#     x  Create a list
+#     x  Add task to list
+#     x  Show all tasks
 #        Read a task from a file
 #        Write a list to a file
 #        Delete a task
@@ -21,19 +21,35 @@ class List
 	def add(task)
 		all_tasks << task
 	end
+
+	def show
+		all_tasks
+	end
 end
 
 class Task
+	## need access to the task item
 	attr_reader :description
 
+	## need to convert the task to a string
+	#   this is always done when a Task is handled
+    def to_s
+		description
+	end
+
+	## this is done only once, upon creation
 	def initialize(description)
 		@description = description
 	end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  list = List.new
-  puts 'You have created a new list'
+	list = List.new
+	puts 'You have created a new list'
+	list.add(Task.new("First Task"))
+	list.add(Task.new("Second Task"))
+	puts 'You have added a Task'
+	puts 'Showing Tasks'
+	## puts because you have converted the tasks in the list to strings
+	puts list.show
 end 
-list.add("First Task")
-puts 'You have added a Task'
